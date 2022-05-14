697e00d70fb3b9c5b2b24e69b68343b805459f31
ignaciano3 <ignaciogp08@gmail.com>
2022-05-13 22:24:26 -0300

    .

--- abb.c
+++ abb.c
@@ -1,6 +1,7 @@
 #include "abb.h"
 #include "pila.h"
 #include <string.h>
+#include "stdio.h"
 
 typedef struct nodo nodo_t;
 
@@ -15,8 +16,7 @@ nodo_t *crear_nodo(const char *clave, void *dato){
     if (nodo == NULL) return NULL;
     nodo->clave = strdup(clave);
     nodo->dato = dato;
-    nodo->izq = NULL;
-    nodo->der = NULL;
+    nodo->izq = nodo->der = NULL;
     return nodo;
 }
 
@@ -41,106 +41,70 @@ size_t abb_cantidad(const abb_t *arbol) {
     return arbol->cantidad;
 }
 
-nodo_t ** buscar_nodo(const abb_t* arbol, const char *clave){
-    // Suponiendo que no se busca la raiz
-    nodo_t *nodo = arbol->raiz;
-
-    while (nodo != NULL) {
-        if (arbol->cmp(nodo->clave, clave) < 0) { // nodo->clave < clave
-            if (nodo->der == NULL || arbol->cmp(nodo->der->clave, clave) == 0){
-                // nodo->der->clave == clave o nodo no existe
-                return &nodo->der;
-            }
-            nodo = nodo->der;
-
-        } else if (arbol->cmp(nodo->clave, clave) > 0){ // nodo->clave > clave o nodo
-            if (nodo->izq == NULL || arbol->cmp(nodo->izq->clave, clave) == 0){
-                // nodo->izq->clave == clave o nodo no existe
-                return &nodo->izq;
-            }
-            nodo = nodo->izq;
-        }
+nodo_t** buscar_nodo(nodo_t **puntero_nodo, const char * clave, abb_comparar_clave_t cmp){
+    if (*puntero_nodo == NULL){
+        return puntero_nodo;
     }
-    return NULL;
+
+    if (cmp(clave, (*puntero_nodo)->clave) < 0){
+        puntero_nodo = buscar_nodo(&(*puntero_nodo)->izq, clave, cmp);
+    } else if (cmp(clave, (*puntero_nodo)->clave) > 0){
+        puntero_nodo = buscar_nodo(&(*puntero_nodo)->der, clave, cmp);
+    }
+
+    return puntero_nodo;
 }
 
+
 bool abb_guardar(abb_t *arbol, const char *clave, void *dato) {
-
-    // caso arbol vacio
-    if (arbol->raiz == NULL){
-        arbol->raiz = crear_nodo(clave, dato);
-        if (arbol->raiz == NULL) return false;
-        arbol->cantidad++;
-        return true;
-    }
-
-    nodo_t **puntero_a_ubicacion;
-    if (arbol->cmp(clave, arbol->raiz->clave) == 0){
-        puntero_a_ubicacion = &arbol->raiz;
-    } else {
-        puntero_a_ubicacion = buscar_nodo(arbol, clave);
-    }
-
-    nodo_t *nodo = *puntero_a_ubicacion;
-    if (nodo == NULL){
-        *puntero_a_ubicacion = crear_nodo(clave, dato);
-        if (*puntero_a_ubicacion == NULL) return false;
+    nodo_t** puntero_nodo = buscar_nodo(&arbol->raiz, clave, arbol->cmp);
+    if (*puntero_nodo == NULL){
+        *puntero_nodo = crear_nodo(clave, dato);
+        if (*puntero_nodo == NULL) return false;
         arbol->cantidad++;
         return true;
     } else {
-        if (arbol->destruir_dato){
-            arbol->destruir_dato(nodo->dato);
-        }
-        nodo->dato = dato;
+        if (arbol->destruir_dato)
+            arbol->destruir_dato((*puntero_nodo)->dato);
+        (*puntero_nodo)->dato = dato;
         return true;
     }
 }
 
 bool abb_pertenece(const abb_t *arbol, const char *clave) {
-
-    if (arbol->raiz == NULL) return false;
-    if (arbol->cmp(arbol->raiz->clave, clave) == 0) return true;
-
-    nodo_t *nodo = *buscar_nodo(arbol, clave);
-    return (nodo != NULL) ? true : false;
+    nodo_t *nodo = arbol->raiz;
+    nodo_t** puntero_nodo = buscar_nodo(&nodo, clave, arbol->cmp);
+    return (*puntero_nodo != NULL) ? true : false;
 }
 
 void *abb_obtener(const abb_t *arbol, const char *clave) {
-
-    if (arbol->raiz == NULL) return NULL;
-    if (arbol->cmp(arbol->raiz->clave, clave) == 0) return arbol->raiz->dato;
-
-    nodo_t *nodo = *buscar_nodo(arbol, clave);
-    return nodo->dato;
+    nodo_t *nodo = arbol->raiz;
+    nodo_t** puntero_nodo = buscar_nodo(&nodo, clave, arbol->cmp);
+    return (*puntero_nodo)->dato;
 }
 
 void *abb_borrar(abb_t *arbol, const char *clave) {
+    nodo_t** puntero_nodo = buscar_nodo(&arbol->raiz, clave, arbol->cmp);
+    if (*puntero_nodo == NULL) return NULL;
 
-    nodo_t **pNodo;
-    if (arbol->cmp(arbol->raiz->clave, clave) == 0){
-        pNodo = &arbol->raiz;
-    } else {
-        pNodo = buscar_nodo(arbol, clave);
-    }
-
-    if (*pNodo == NULL) return NULL;
-
-    nodo_t *nodo = *pNodo;
+    nodo_t *nodo = *puntero_nodo;
     void* dato = nodo->dato;
-
     arbol->cantidad--;
+
     if (nodo->izq !=NULL && nodo->der == NULL){
-        *pNodo = nodo->izq;
+        *puntero_nodo = nodo->izq;
     } else if (nodo->der != NULL && nodo->izq == NULL){
-        *pNodo = nodo->der;
+        *puntero_nodo = nodo->der;
     } else if (nodo->izq != NULL && nodo->der != NULL){
         // cuando tengo hijo izq y der
         exit(1);
     }
+
     free(nodo->clave);
     free(nodo);
     return dato;
 }
+
 #include "stdio.h"
 void inorder(nodo_t *nodo, bool visitar(const char *, void *, void *), void *extra){
     if (nodo == NULL) return;
@@ -156,7 +120,7 @@ void abb_in_order(abb_t *arbol, bool visitar(const char *, void *, void *), void
 
 void abb_destruir(abb_t *arbol) {
     while (arbol->cantidad > 0){
-        void * dato = abb_borrar(arbol, arbol->raiz->clave);
+        void *dato = abb_borrar(arbol, arbol->raiz->clave);
         if (arbol->destruir_dato)
             arbol->destruir_dato(dato);
     }
